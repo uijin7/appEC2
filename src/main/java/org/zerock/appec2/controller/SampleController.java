de0303b62629115a80a5731bf20b5fa5c8e32e34
uijin7 <hwanguijin7@gmail.com>
2023-06-22 14:37:21 +0900

    2

--- src/main/java/org/zerock/appec2/controller/SampleController.java
+++ src/main/java/org/zerock/appec2/controller/SampleController.java
@@ -1,15 +1,20 @@
 package org.zerock.appec2.controller;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/sample")
 public class SampleController {
 
     @GetMapping("/getArr")
     public String[] getArr() {
+
+        log.info("INFO");
 
         return new String[]{"AAA", "BBB", "CCC"};
     }
